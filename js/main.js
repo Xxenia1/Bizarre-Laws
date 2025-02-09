3d54f9be75a19ae6e400e1201ef5412f7acb3eb7
Xxenia1 <157653332+Xxenia1@users.noreply.github.com>
2025-02-08 18:04:38 -0600

    data create

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 var map = L.map('map').setView([20, 0], 2); // Center at a global level
 
 // Add OpenStreetMap as the basemap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '© OpenStreetMap contributors © CARTO',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }).addTo(map);
