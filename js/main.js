eac5c2a18e78c08a1c7a86e113ad974c2cdf3d86
Xxenia1 <157653332+Xxenia1@users.noreply.github.com>
2025-02-08 18:36:42 -0600

    zoom in and click

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   }).addTo(map);
 
 //load continent boundaries from geojson
-/*fetch('Data/continents.json') // Update with the correct file path
+fetch('Data/continents.json') // Update with the correct file path
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
             style: function(feature) {
-                return { color: "grey", weight: 1, fillOpacity: 0.2 };
+                return { color: "transparent", weight: 1, fillOpacity: 0.2 };
             },
             onEachFeature: function(feature, layer) {
                 // Highlight effect on hover
@@ -22,18 +22,19 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
                     this.setStyle({ fillOpacity: 0.5, color: "red" });
                 });
                 layer.on('mouseout', function() {
-                    this.setStyle({ fillOpacity: 0.2, color: "grey" });
+                    this.setStyle({ fillOpacity: 0.2, color: "transparent" });
                 });
                 layer.on('click', function() {
-                    map.fitBounds(layer.getBounds()); // Zoom into continent on click
+                    let bounds = layer.getBounds(); // Get bounding box of continent
+                    map.flyToBounds(bounds); // Zoom into continent on click
                 });
             }
         }).addTo(map);
     })
-    .catch(error => console.error("Error loading GeoJSON:", error));*/
+    .catch(error => console.error("Error loading GeoJSON:", error));
 
-// Define continent boundaries (simplified example, use GeoJSON for accuracy)
-var continents = {
+// Define continent boundaries 
+/*var continents = {
     "Africa": {bounds: [[-35, -20], [37, 55]], center: [0, 20]},
     "Asia": {bounds: [[-10, 60], [55, 150]], center: [30, 100]},
     "Europe": {bounds: [[35, -25], [70, 45]], center: [55, 15]},
@@ -55,4 +56,4 @@ for (let continent in continents) {
     layer.on('click', function() {
         map.flyTo(continents[continent].center, 4); // Zoom to continent
     });
-}
+}*/
